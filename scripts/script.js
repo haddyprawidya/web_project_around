d65a9a887941aa57d9a42f40beefd8cb2fcb69f3
haddyprawidya <haddyprawidya@gmail.com>
2023-09-11 17:20:17 +0800

    feat: new Section Cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,28 @@
-const main = document.querySelector('#main');
-const editButton = main.querySelector('.profile__edit-button');
-const closeButton = main.querySelector('.popup__close');
-const editProfile = main.querySelector('.popup');
-
+const main = document.querySelector("#main");
+const editButton = main.querySelector(".profile__edit-button");
+const closeButton = main.querySelector(".popup__close");
+const editProfile = main.querySelector(".popup");
 
 function popupToggle() {
-    editProfile.classList.toggle('popup__opened');
-};
+  editProfile.classList.toggle("popup__opened");
+}
 
-closeButton.addEventListener('click', popupToggle);
-editButton.addEventListener('click', popupToggle);
+closeButton.addEventListener("click", popupToggle);
+editButton.addEventListener("click", popupToggle);
 
-
-const profilName = main.querySelector('.profile__name');
-const profilAbout = main.querySelector('.profile__about');
-const inputName = main.querySelector('.popup__input_name');
-const inputDescription = main.querySelector('.popup__input_about');
-const submitButton = main.querySelector('.popup__submit');
+const profilName = main.querySelector(".profile__name");
+const profilAbout = main.querySelector(".profile__about");
+const inputName = main.querySelector(".popup__input_name");
+const inputDescription = main.querySelector(".popup__input_about");
+const submitButton = main.querySelector(".popup__submit");
 
 inputName.value = profilName.textContent;
 inputDescription.value = profilAbout.textContent;
 
-submitButton.addEventListener('click', function(evt){
-    evt.preventDefault();
-    profilName.textContent = inputName.value;
-    profilAbout.textContent = inputDescription.value;
-    
-    popupToggle();
-});
+submitButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  profilName.textContent = inputName.value;
+  profilAbout.textContent = inputDescription.value;
 
+  popupToggle();
+});

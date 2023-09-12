f7ef5c636ddde0af3240cc9dd8b5696c1eb56568
haddyprawidya <haddyprawidya@gmail.com>
2023-09-12 17:25:15 +0800

    feat(submitButton) change disable

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,13 @@
 const main = document.querySelector("#main");
 const editButton = main.querySelector(".profile__edit-button");
-const closeButton = main.querySelector(".popup__close");
 const editProfile = main.querySelector(".popup");
+const closeButton = editProfile.querySelector(".popup__close");
 
 function popupToggle() {
   editProfile.classList.toggle("popup__opened");
+  inputName.value = profilName.textContent;
+  inputDescription.value = profilAbout.textContent;
+  submitButton.disabled = true;
 }
 
 closeButton.addEventListener("click", popupToggle);
@@ -12,12 +15,23 @@ editButton.addEventListener("click", popupToggle);
 
 const profilName = main.querySelector(".profile__name");
 const profilAbout = main.querySelector(".profile__about");
-const inputName = main.querySelector(".popup__input_name");
-const inputDescription = main.querySelector(".popup__input_about");
-const submitButton = main.querySelector(".popup__submit");
+const inputName = editProfile.querySelector(".popup__input_name");
+const inputDescription = editProfile.querySelector(".popup__input_about");
+const submitButton = editProfile.querySelector(".popup__submit");
 
-inputName.value = profilName.textContent;
-inputDescription.value = profilAbout.textContent;
+function changeForm() {
+  if (
+    inputName.value !== profilName.textContent ||
+    inputDescription.value !== profilAbout.textContent
+  ) {
+    submitButton.disabled = false;
+    if (inputName.value === "" || inputDescription.value === "") {
+      submitButton.disabled = true;
+    }
+  }
+}
+
+editProfile.addEventListener("input", changeForm);
 
 submitButton.addEventListener("click", function (evt) {
   evt.preventDefault();
